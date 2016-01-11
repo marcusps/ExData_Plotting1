9840ee47c7b9bdd7382420f363b8612cd7d9c1ee
Marcus P S <marcus.ps@gmail.com>
2016-01-10 22:25:55 -0500

    Data loading file

---
+++ load.data.R
@@ -0,0 +1,12 @@
+# Assignment #1
+
+library(data.table)
+
+data.url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(data.url, "exdata data household_power_consumption.zip", mode="wb")
+unzip("exdata data household_power_consumption.zip")
+data.file <- "household_power_consumption.txt"
+
+data.a1 <- fread(data.file, sep = ";", header = TRUE, na.strings = "?")
+data.a1$Date <- as.Date( data.a1$Date, "%d/%m/%Y")
+our.data <- subset(data.a1, Date == as.Date( "01/02/2007", "%d/%m/%Y") | Date == as.Date( "02/02/2007", "%d/%m/%Y"))
