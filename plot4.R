70368f5325b898abd864d16e04a9cffd772a3cf0
Marcus P S <marcus.ps@gmail.com>
2016-01-10 23:22:08 -0500

    Fourth plot

---
+++ plot4.R
@@ -0,0 +1,31 @@
+source("load.data.R")
+
+png("plot4.png", width = 480, height=480, units = "px")
+
+par(mfrow=c(2,2))
+
+with(our.data, {
+  data.time <- strptime(paste(Date, Time), format="%Y-%m-%d %H:%M:%S")
+  plot(data.time, Global_active_power, type="l", ylab = "Global Active Power (kilowatts)", xlab="")
+})
+
+with(our.data, {
+  data.time <- strptime(paste(Date, Time), format="%Y-%m-%d %H:%M:%S")
+  plot(data.time, Voltage, type="l", ylab = "Voltage", xlab="datetime")
+})
+
+with(our.data, {
+  data.time <- strptime(paste(Date, Time), format="%Y-%m-%d %H:%M:%S")
+  plot(data.time, Sub_metering_1, type="l", ylab = "Energy sub metering", xlab="")
+  lines(data.time, Sub_metering_2, type="l", ylab = "Energy sub metering", xlab="", col="red")
+  lines(data.time, Sub_metering_3, type="l", ylab = "Energy sub metering", xlab="", col="blue")
+})
+legend("topright", bty = "n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
+
+with(our.data, {
+  data.time <- strptime(paste(Date, Time), format="%Y-%m-%d %H:%M:%S")
+  plot(data.time, Global_reactive_power, type="l", xlab="datetime")
+})
+
+dev.off()
+
